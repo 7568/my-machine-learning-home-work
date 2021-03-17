2814fb23074373d9ab48d992270837362d0e59b0
7568 <18508227568@163.com>
2021-03-17 21:02:56 +0800

    homework 2

--- w2/gradient_descent_2.py
+++ w2/gradient_descent_2.py
@@ -21,16 +21,16 @@ Description:
 
 import numpy as np
 import matplotlib.pyplot as plt
-import math
+import random
 
 
-def f(x, y, a, b):
+def loss(x, y, a, b):
     """
     损失函数
     y:样本数据对应的函数
     a,b都是与x，y长度一样的数组
     """
-    l = (1 / 2) * 1 / (1 + np.power(math.e, -(y - a * x - b) ** 2))
+    l = (1 / 2) * np.sum(np.power(3.0 * x + 4 - a * x - b, 2))
     # print('l = ', l)
     return l
 
@@ -39,14 +39,14 @@ def loss_d_f_a(x, y, a, b):
     """
     损失函数对a求倒
     """
-    return f(x, y, a, b) * (1 - f(x, y, a, b)) * 2 * (y - a * x - b) * (-x)
+    return a * np.sum(np.power(x, 2)) - np.sum((y - b) * x)
 
 
 def loss_d_f_b(x, y, a, b):
     """
     损失函数对b求倒
     """
-    return f(x, y, a, b) * (1 - f(x, y, a, b)) * 2 * (y - a * x - b) * (-1)
+    return len(x) * b - np.sum(y - a * x)
 
 
 def loss_d_f(x, y, a, b):
@@ -56,44 +56,41 @@ def loss_d_f(x, y, a, b):
     dl/da = loss*(1-loss)*2*(y-y')*x
     dl/da = loss*(1-loss)*2*(y-y')
     """
-    l_d = np.array([np.sum(loss_d_f_a(x, y, a, b)), np.sum(loss_d_f_b(x, y, a, b))])
+    l_d = np.array([loss_d_f_a(x, y, a, b) / len(x), loss_d_f_b(x, y, a, b) / len(x)])
     # print('l_d = ', l_d)
     return l_d
 
 
 # 产生原始数据，并添加干扰
 def create_data():
-    d = [np.arange(-100, 100, 2), 3.0 * np.arange(-100, 100, 2) + 4.0 + (np.random.sample(100)*2-1)*30]
+    d = [np.arange(-100, 100, 2), 3.0 * np.arange(-100, 100, 2) + 4.0 + (np.random.sample(100) * 2 - 1)]
     return d
 
 
 xs = create_data()
 
-print(xs)
-plt.scatter(xs[0], xs[1],s=2)
+print(len(xs))
+plt.scatter(xs[0], xs[1], s=2)
 
-learning_rate = 0.00001
-max_loop = 5000
+learning_rate = [0.0001, 0.01]
+max_loop = 50000
 tolerance = 0.001
-a_init = 2.0
-b_init = 15.0
-
+a_init = random.random()
+b_init = random.random()
+a_b = [a_init, b_init]
 fx_pre = 0
+count = 0
 for i in range(max_loop):
-    a_b = [np.repeat(a_init, 100), np.repeat(b_init, 100)]
+    count += 1
     d_f_x = loss_d_f(xs[0], xs[1], a_b[0], a_b[1])
-    # print('d_f_x = ', d_f_x)
-    # d_f_x = d_f_2(f,x)
-    [a_init, b_init] = [a_init, b_init] - learning_rate * d_f_x
-    # print([a_init, b_init])
-    # a_b = [np.repeat(a_init, 100), np.repeat(b_init, 100)]
-    # fx_cur = np.sum(f(xs[0], xs[1], a_b[0], a_b[1]))
-    # if abs(fx_cur - fx_pre) < tolerance:
-        # break
-    # fx_pre = fx_cur
-print('initial a = ', a_init, 'b = ', b_init)
-# print('arg min loss of a_b = ', a_b)
-a_b = [np.repeat(a_init, 100), np.repeat(b_init, 100)]
-print('loss = ', np.sum(f(xs[0], xs[1], a_b[0], a_b[1])))
-plt.plot(xs[0], a_init*xs[0]+b_init)
+    a_b -= learning_rate * d_f_x
+    print(a_b)
+    fx_cur = loss(xs[0], xs[1], a_b[0], a_b[1])
+    if abs(fx_cur - fx_pre) < tolerance:
+        break
+    fx_pre = fx_cur
+print('time of loop : ', count)
+print('a = ', a_b[0], 'b = ', a_b[1])
+print('loss = ', loss(xs[0], xs[1], a_b[0], a_b[1]))
+plt.plot(xs[0], a_b[0] * xs[0] + a_b[1])
 plt.show()
